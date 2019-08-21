33d38b96178ced64156956ce77a94d7adb9cd02a
3mn <nfatah41@gmail.com>
2019-08-21 19:45:11 +0300

    add a welcome page

---
+++ .storybook/welcomeStory.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('Welcome', module).add(
+	'Our new storybook of components',
+	() => <h1>Welcome to our storybook</h1>
+)
